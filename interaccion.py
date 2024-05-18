ae9c811974fb0a174c47c51208c245f64eb2a162
jair <jaircachays05@gmail.com>
2024-05-18 17:54:14 -0500

    cambios mas

--- interaccion.py
+++ interaccion.py
@@ -69,21 +69,22 @@ def jugar_humano_humano():
 def jugar_humano_maquina(nivel):
     pygame.init()
     pantalla = pygame.display.set_mode((COLUMNAS * TAMANO + MARGEN_DER, FILAS * TAMANO + MARGEN_SUP))
-    pygame.display.set_caption("Gravity-HvsH")
+    pygame.display.set_caption("Gravity-HvsM")
     tablero=Tablero(FILAS,COLUMNAS)
     jugadorX=Jugador(1)
     jugadorO=JugadorIA(2,nivel)
     turno='x'
     dibujar_escenario(tablero,jugadorX,jugadorO,turno)
     juego_en_curso = True
+    clock = pygame.time.Clock()#ahora
     
     while juego_en_curso:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        if turno =='x':
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
                 
-            if turno =='x':
                 if evento.type == pygame.MOUSEBUTTONDOWN:
                     posx = evento.pos[0]
                     if posx<TAMANO*COLUMNAS:
@@ -95,32 +96,34 @@ def jugar_humano_maquina(nivel):
                             jugadorX.colocar_ficha(tablero,columna)
                             turno = 'o'
                             dibujar_escenario(tablero,jugadorX,jugadorO,turno)
-                            resultado=tablero.comprobar_resultado()
-                            if resultado != "Continua":  # Revisar si el jugador actual ha ganado después de girar
-                                juego_en_curso = False
-                                juego_terminado(resultado)
+                            break
 
                     elif evento.button == 3:  # Click derecho para borrar columna
                         if jugadorX.borrarPosible():
                             jugadorX.borrar_columna(tablero,columna)
                             turno = 'o'
                             dibujar_escenario(tablero,jugadorX,jugadorO,turno)
+                            break
 
-                    if evento.type == pygame.KEYDOWN:
-                        if evento.key == pygame.K_r:  # Presionar 'r' para rotar el tablero
-                            jugadorX.rotar_tablero(tablero)
-                            turno = 'o'
-                            dibujar_escenario(tablero,jugadorX,jugadorO,turno)
-                            resultado=tablero.comprobar_resultado()
-                            if resultado != "Continua":  # Revisar si el jugador actual ha ganado después de girar
-                                juego_en_curso = False
-                                juego_terminado(resultado)
-            else:
-                jugadorO.decidir(tablero, turno, jugadorX)
-                turno = 'x'
-                dibujar_escenario(tablero,jugadorX,jugadorO,turno)
-                resultado=tablero.comprobar_resultado()
-                if resultado != "Continua":  # Revisar si el jugador actual ha ganado después de girar
-                    juego_en_curso = False
-                    juego_terminado(resultado)
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_r:  # Presionar 'r' para rotar el tablero
+                        jugadorX.rotar_tablero(tablero)
+                        turno = 'o'
+                        dibujar_escenario(tablero,jugadorX,jugadorO,turno)
+                        break
+
+        else:
+            jugadorO.decidir(tablero, turno, jugadorX)
+            turno = 'x'
+            dibujar_escenario(tablero,jugadorX,jugadorO,turno)
+
+
+        pygame.display.update()
+        clock.tick(30)
+        resultado=tablero.comprobar_resultado()
+        if resultado != "Continua":  # Revisar si el jugador actual ha ganado después de girar
+            juego_en_curso = False
+            juego_terminado(resultado)
+
+
     return "MENU"
